1bf36d108219f2f3d752afe6587415a277c2d978
Fassbender <gfassbender@graphodata.lan>
2018-05-02 12:18:02 +0200

    initial commit

---
+++ LinkedList.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+template<typename T>
+class LinkedList{
+    struct Node {
+        T value;
+        Node *next;
+        int index;
+    };
+
+    int nodeIndex;
+    Node *start;
+    Node *head;
+
+public:
+    LinkedList(){
+        head = NULL;
+    }
+
+    ~LinkedList(){
+        Node *next = head;
+        while(next) {
+            Node *deleteMe = next;
+            next = next->next;
+            delete deleteMe;
+        }
+    }
+
+    void push(T val){
+        Node *n = new Node();
+        n->value = val;
+        n->index = nodeIndex++;
+        if (n->index == 0) start = n;
+        n->next = head;
+        head = n;
+    }
+
+    T pop(){
+        Node *n = head;
+        T ret = n->value;
+        head = head->next;
+        delete n;
+        return ret;
+    }
+
+    // TODO
+    void forEach(void <lambda(int)> fn) const {
+        Node * node = head;
+        while (node) {
+            fn(*node);
+            node = node->next;
+        }
+    }
+
+    T operator[](int &x) const {
+        if (x < 0 ||x > nodeIndex) return NULL;
+        Node *currentNode = start;
+        while (currentNode->index != x) {
+            currentNode = currentNode->next;
+        }
+        return currentNode->value;
+    }
+
+    void operator<<(T x) {
+        push(x);
+    }
+};
+
+int main() {
+    LinkedList<int> list;
+
+    list << 5;
+    list << 10;
+    list << 20;
+
+    std::cout << list.pop() << std::endl;
+    std::cout << list.pop() << std::endl;
+    std::cout << list.pop() << std::endl;
+    std::cin.get();
+    return 0;
+}
